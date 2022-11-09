7d03b8d14455f40bdb9f3fbcca76086c92d4727a
gaston becerra <gaston.becerra@gmail.com>
2022-11-09 01:03:32 -0300

    solo 2 2dos

--- standalone/standalone.js
+++ standalone/standalone.js
@@ -2,14 +2,16 @@
 // vars and default values
 // ------------------------------------------------------------------------------------------------------
 
-//2do: all these should be monitored; and on changes, show in dump_output
-
 var output = [];
 var current_document_i = false; 
 var last_used_code = false;
 var last_coded_document_i = false;
+var last_coded_fragment_id = false;
 var selected_text = false;
+var selected_range_start = false;
+var selected_range_end = false;
 var current_fragment = false;
+var current_fragment_id = false;
 
 var show_monitor = true; //2do: deberia arrancar apagado
 var codification_user = false;  //2do: esto deberia venir de R
@@ -20,8 +22,12 @@ function dump_output() {
         "current_document_i: " + current_document_i + "<br>" +
         "last_used_code: " + last_used_code + "<br>" +
         "last_coded_document_i: " + last_coded_document_i + "<br>" +
+        "last_coded_fragment_id: " + last_coded_fragment_id + "<br>" +
         "selected_text: " + selected_text + "<br>" +
+        "selected_range_start: " + selected_range_start + "<br>" +
+        "selected_range_end: " + selected_range_end + "<br>" +
         "current_fragment: " + current_fragment + "<br>" +
+        "current_fragment_id: " + current_fragment_id + "<br>" +
 
         "show_monitor: " + show_monitor + "<br>" +
         "codification_user: " + codification_user + "<br>" +
@@ -84,9 +90,11 @@ function set_document_annotations( document_i , code_i = false , memo = false , 
             if (memo) {
                 output.document_annotations[i].memo = memo;
             }
+            if (memo=="") {
+                output.document_annotations[i].memo = "";
+            }
             
             if (selected_tokens) {
-                //2do: update tokens
             }
 
             output.document_annotations[i].codification_date = new Date();
@@ -105,7 +113,7 @@ function set_document_annotations( document_i , code_i = false , memo = false , 
             "document": document_i,
             "codes": code_i,
             "memo": memo,
-            "selected_tokens": [],  //2do: insert selected tokens
+            "selected_tokens": [],  //v2
             "codification_date": new Date(),
             "codification_user": codification_user,
         });
@@ -137,16 +145,19 @@ function get_document_memo( document_i ) {
 
 function set_fragment( document_i , fragment_text ) {
     document_i = parseInt(document_i);
+    var id = idx();
     output.fragment_annotations.push({
+        "id": id,
         "document": document_i,
         "text": fragment_text,
-        "start": null,  //2do: se puede tomar? o llegado el caso se puede ampliar la seleccion programaticamente?
-        "end": null,    //2do: se puede tomar?
+        "start": selected_range_start,
+        "end": selected_range_end,
         "codes": [],
         "memo": "",
         "codification_date": new Date(),
         "codification_user": codification_user
     });
+    return(id);
 }
 
 function delete_fragment( document_i , fragment_text ) {
@@ -158,20 +169,20 @@ function delete_fragment( document_i , fragment_text ) {
     }
 }
 
-function set_fragment_annotations( document_i, fragment_text = false , code_i = false , memo = false ) {
+function set_fragment_annotations( document_i, fragment_id = false , code_i = false , memo = false  ) {
 
-    // console.log("set_fragment_annotations: " + document_i + " " + fragment_text + " " + code_i + " " + memo);
     document_i = parseInt(document_i);
 
     for (var i = 0; i < output.fragment_annotations.length; i++) {
-        if (output.fragment_annotations[i].document == document_i && output.fragment_annotations[i].text == fragment_text) {
-            
+        if (output.fragment_annotations[i].document == document_i && output.fragment_annotations[i].id == fragment_id) {
+           
             if (code_i) {
                 code_i = parseInt(code_i);    
                 if ( output.fragment_annotations[i].codes.indexOf( code_i ) === -1 ) {
                     output.fragment_annotations[i].codes.push( code_i );
                     last_used_code = code_i;
                     last_coded_document_i = document_i;
+                    last_coded_fragment_id = fragment_id;
                 } else {
                     output.fragment_annotations[i].codes.splice( output.fragment_annotations[i].codes.indexOf( code_i ), 1 );
                 }
@@ -180,13 +191,15 @@ function set_fragment_annotations( document_i, fragment_text = false , code_i = 
             if (memo) {
                 output.fragment_annotations[i].memo = memo;
             }
+            if (memo=="") {
+                output.document_annotations[i].memo = "";
+            }
             
             output.fragment_annotations[i].codification_date = new Date();
             output.fragment_annotations[i].codification_user = codification_user; 
 
         }
     }
-
     dump_output();
 
 }
@@ -201,20 +214,20 @@ function get_fragments( document_i ) {
     return fragments;
 }
 
-function get_fragment_codes( document_i , fragment_text ) {
+function get_fragment_codes( document_i , fragment_id ) {
     var fragment_codes = [];
     for (var i = 0; i < output.fragment_annotations.length; i++) {
-        if (output.fragment_annotations[i].document == document_i && output.fragment_annotations[i].text == fragment_text) {
+        if (output.fragment_annotations[i].document == document_i && output.fragment_annotations[i].id == fragment_id) {
             fragment_codes.push(output.fragment_annotations[i].codes);
         }
     }
     return fragment_codes;
 }
 
-function get_fragment_memo( document_i , fragment_text ) {
+function get_fragment_memo( document_i , fragment_id ) {
     var fragment_memo = "";
     for (var i = 0; i < output.fragment_annotations.length; i++) {
-        if (output.fragment_annotations[i].document == document_i && output.fragment_annotations[i].text == fragment_text) {
+        if (output.fragment_annotations[i].document == document_i && output.fragment_annotations[i].id == fragment_id) {
             fragment_memo = output.fragment_annotations[i].memo;
         }
     }
@@ -223,6 +236,13 @@ function get_fragment_memo( document_i , fragment_text ) {
 
 function read_input_data( input ) {
     output = input;
+}
+
+function clean_selection() {
+    selected_text = false;
+    selected_range_start = false;    
+    selected_range_end = false;
+    dump_output();
 }
 
 // ------------------------------------------------------------------------------------------------------
@@ -315,7 +335,7 @@ function document_annotation_panel() {
             '<div class="document_code_item_automatic" code_behaviour="last">Last used code</div>' + 
             '<div class="document_code_item_automatic" code_behaviour="open">Open code</div>' +
             '<div class="document_code_item_automatic" code_behaviour="invivo">Invivo code</div>' +
-            // '<div class="document_code_item_automatic" style="background-color: red">Select tokens/features</div>' +   //2do: que se pueda seleccionar palabras, para feature engeenering
+            // '<div class="document_code_item_automatic" style="background-color: red">Select tokens/features</div>' +   //v2
             '</div>' );
 
         annotate_doc_panel.append('<textarea id="document_memo" placeholder="Include memos to this document...">' + get_document_memo(current_document_i) + '</textarea>');
@@ -342,19 +362,23 @@ function fragment_annotation_panel() {
         
         annotate_fragment_panel.append('<div id="fragments_navigation"></div>');
         var fragments_navigation = $("#fragments_navigation");
+        
         var fragments = get_fragments(current_document_i);
-        for (var i = 0; i < fragments.length; i++) {    //2do: al final voy a usar frament_i? o conviene usar un hash?
-            if (current_fragment && fragments[i].text == current_fragment) {
+
+        for (var i = 0; i < fragments.length; i++) {    
+            if (current_fragment_id && fragments[i].id == current_fragment_id ) { 
                 var fragment_class = "selected_item_fragment";
             } else {
                 var fragment_class = "unselected_item";
             }
-            fragments_navigation.append( '<div class="fragment_navigation_item ' + fragment_class + '" fragment_i="' + i + '" fragment="' + fragments[i].text + '">' +
+            fragments_navigation.append( '<div class="fragment_navigation_item ' + fragment_class + 
+                '" fragment_id="' + fragments[i].id +
+                '" fragment="' + fragments[i].text + '">' +
             fragments[i].text.substring(0, 25) + 
             '...</div>' );
         }
 
-        if (current_fragment === false) {} else {
+        if (current_fragment_id === false) {} else { 
             annotate_fragment_panel.append('<div id="delete_fragment_wraper"></div>');
             var delete_fragment_wraper = $("#delete_fragment_wraper");
             delete_fragment_wraper.append('<div id="delete_fragment_button">Delete fragment</div>');
@@ -363,11 +387,16 @@ function fragment_annotation_panel() {
             var fragment_code_navigation = $("#fragment_code_navigation");
             
             codes = get_codes();
-            fragment_codes = get_fragment_codes(current_document_i, current_fragment);
-
-            for (var i = 0; i < codes.length; i++) {
-                if ( fragment_codes[0].indexOf(i) === -1 ) { var code_class = "unselected_item"; } else { var code_class = "selected_item"; } 
-                fragment_code_navigation.append( '<div class="fragment_code_item ' + code_class + '" code_i="' + i + '">' + codes[i] + '</div>' );
+            fragment_codes = get_fragment_codes(current_document_i, current_fragment_id);
+            if ( codes && fragment_codes && fragment_codes.length > 0 ) {
+                for (var i = 0; i < codes.length; i++) {
+                    if ( fragment_codes[0].indexOf(i) === -1 ) { 
+                        var code_class = "unselected_item"; 
+                    } else { 
+                        var code_class = "selected_item"; 
+                    } 
+                    fragment_code_navigation.append( '<div class="fragment_code_item ' + code_class + '" code_i="' + i + '">' + codes[i] + '</div>' );
+                }
             }
     
             annotate_fragment_panel.append( '<div id="fragment_code_item_automatics">' + 
@@ -376,7 +405,8 @@ function fragment_annotation_panel() {
                 '<div class="fragment_code_item_automatic" code_behaviour="invivo">Invivo code</div>' +
                 '</div>' );
 
-            annotate_fragment_panel.append('<textarea id="fragment_memo" placeholder="Include memos for this fragment...">' + get_fragment_memo( current_document_i, current_fragment ) + '</textarea>');
+            annotate_fragment_panel.append('<textarea id="fragment_memo" placeholder="Include memos for this fragment...">' + 
+                get_fragment_memo( current_document_i, current_fragment_id ) + '</textarea>');
         }
     }
 }
@@ -414,7 +444,7 @@ $(document).on('click', '.document_code_item', function() {
     } else {
         var code_i = $(this).attr("code_i"); 
         set_document_annotations( current_document_i , code = code_i );
-        last_used_code = code_i;    //2do: esto deberia estar dentro de set_document_annotations
+        last_used_code = code_i;
         document_annotation_panel(); 
         fragment_annotation_panel();
     }
@@ -444,7 +474,7 @@ $(document).on('click', '.document_code_item_automatic', function() {
             if ( selected_text != false && selected_text.trim() != "" ) {
                 var code_i = set_new_code( selected_text.trim() );
                 set_document_annotations( current_document_i , code = code_i );
-                selected_text = false;
+                clean_selection();
             } else {
                 alert('Select some text first');
             }
@@ -464,22 +494,22 @@ $(document).on('click', '.fragment_code_item_automatic', function() {
         
         var code_behaviour = $(this).attr("code_behaviour");
         if (code_behaviour == "last") { 
-            if ( last_coded_document_i != current_document_i ) {
-                set_fragment_annotations( current_document_i, current_fragment, code = last_used_code );
+            if ( last_coded_fragment_id != current_fragment_id ) {
+                set_fragment_annotations( document_i = current_document_i, fragment_id = current_fragment_id, code_i = last_used_code );
             }
         }
         if (code_behaviour == "open") {
             var code = prompt("Please enter your code", "");
             if (code != null) {
                 var code_i = set_new_code( code );
-                set_fragment_annotations( current_document_i, current_fragment, code = code_i );
+                set_fragment_annotations( document_i = current_document_i, fragment_id = current_fragment_id, code_i = code_i );
             }        
         }
         if (code_behaviour == "invivo") {   
-            if ( selected_text != false && selected_text.trim() != "" ) {   //2do: invivo deberia repetir el texto seleccionado?
+            if ( selected_text != false && selected_text.trim() != "" ) { 
                 var code_i = set_new_code( selected_text.trim() );
-                set_fragment_annotations( current_document_i, current_fragment, code = code_i );
-                selected_text = false;
+                set_fragment_annotations( document_i = current_document_i, fragment_id = current_fragment_id, code_i = code_i );
+                clean_selection();
             } else {
                 alert('Select some text first');
             }
@@ -497,12 +527,12 @@ $(document).on('click', '#create_fragment_button', function() {
     } else {
         var fragment = selected_text.trim();
         if (fragment != "") {
-            set_fragment( 
+            current_fragment_id = set_fragment( 
                 document_i = current_document_i,
                 fragment_text = fragment
             );
             current_fragment = selected_text;
-            selected_text = false;
+            clean_selection();
             fragment_annotation_panel(); 
             dump_output();
         }
@@ -510,12 +540,12 @@ $(document).on('click', '#create_fragment_button', function() {
 });
 
 $(document).on('click', '.fragment_code_item', function() {
-    if (current_fragment === false) {
+    if (current_fragment_id === false) {
         alert('Select a fragment first');
     } else {
         var code_i = $(this).attr("code_i"); 
-        set_fragment_annotations( document_i = current_document_i, fragment_text = current_fragment, code_i = code_i ); //2do: se loopea por fragment_i?
-        last_used_code = code_i;    //2do: esto deberia estar dentro de set_document_annotations
+        set_fragment_annotations( document_i = current_document_i, fragment_id = current_fragment_id, code_i = code_i );
+        last_used_code = code_i;
         fragment_annotation_panel();
     }
     dump_output();
@@ -523,22 +553,23 @@ $(document).on('click', '.fragment_code_item', function() {
 
 $(document).on('click', '.fragment_navigation_item', function() {
     var fragment = $(this).attr("fragment");
-    current_fragment = fragment;
+    current_fragment_id = $(this).attr("fragment_id");
+    // current_fragment = fragment;
+    // selected_range_start = $(this).attr("fragment_start");
+    // selected_range_end = $(this).attr("fragment_end");
     fragment_annotation_panel();
     dump_output();
 });
 
 $(document).on('change', '#document_memo', function() {
-    //2do: no anda si borro el texto (limpiar memo)
     var memo = $(this).val();
     set_document_annotations( current_document_i, code = false, memo = memo );
     dump_output();
 });
 
 $(document).on('change', '#fragment_memo', function() {
-    //2do: no anda si borro el texto (limpiar memo)?
     var memo = $(this).val();
-    set_fragment_annotations( document_i = current_document_i, fragment_text = current_fragment, code_i = false, memo = memo );
+    set_fragment_annotations( document_i = current_document_i, fragment_id = current_fragment_id, code_i = false, memo = memo );
     dump_output();
 });
 
@@ -569,24 +600,27 @@ $(document).on('click', '#export', function() { //2do: es mas o menos por aca
     $("#export").download = "output.json";
 });
 
-
 $(document).on('mouseup', '#document_viewer', function() {
     if (current_document_i === false) {
     } else {
-        selected_text = getSelectedText();
+        selected_text = window.getSelection().toString();   // https://developer.mozilla.org/en-US/docs/Web/API/Selection
+        var range = window.getSelection().getRangeAt(0);
+        selected_range_start = range.startOffset;   // https://stackoverflow.com/questions/50843340/window-getselection-getrange0-does-not-work-when-text-is-wrapped-by-mark
+        selected_range_end = range.endOffset;
         dump_output();
     }
 });
 
-function getSelectedText() {
-    if (window.getSelection) {
-        return window.getSelection().toString();
-    } else if (document.selection) {
-        return document.selection.createRange().text;
-    }
-    return '';
-}
-
 window.onbeforeunload = function(e) {   // trigger alert on windown close
 //   return 'Please make sure to save your work before leaving this page.';
+};
+
+// ------------------------------------------------------------------------------------------------------
+// aux fns
+// ------------------------------------------------------------------------------------------------------
+
+const idx = function() {
+    return Math.random()
+      .toString(36)
+      .substr(2, 8);
 };
